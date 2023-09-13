694b022dba6bf126564d8a30757fda2753bbf737
Daniel Castillo <Daniel.302009@hotmail.com>
2023-09-13 12:02:50 -0500

    update cardGalery

--- components/galery/cardGalery.jsx
+++ components/galery/cardGalery.jsx
@@ -1,10 +1,10 @@
 const CardGalery = ({ image, title }) => {
   return (
-    <div className="bg-white rounded-xl p-8 shadow-xl hover:shadow-2xl">
+    <div className="bg-white rounded-xl p-2 shadow-xl hover:shadow-2xl">
       <div className="relative h-96">
-        <div className={`bg-cover bg-center h-3/4 ${image}`}></div>
-        <div className="flex flex-col justify-between pt-12 p-6  bottom-0 left-0 w-full text-center h-1/4">
-          <h2 className="text-lg md:text-xl text-gray-700 font-bold mb-2 md:mb-0 md:text-center">{title}</h2>
+        <div className={`bg-cover bg-center rounded-xl h-3/4 ${image}`}></div>
+        <div className="flex flex-col justify-center content-center mt-2 pt-8 px-6 bottom-0 left-0 w-full text-center">
+          <h2 className="text-lg md:text-xl text-gray-700 font-bold">{title}</h2>
         </div>
       </div>
     </div>
